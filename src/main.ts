04ac08bd5e91a3ee733d105bff5f12afae07cd7a
mohit kumar <mohitkumarjangra01@gmail.com>
2025-08-23 19:09:23 +0530

    added

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import 'tsconfig-paths/register';
+
   import { NestFactory } from '@nestjs/core';
   import { AppModule } from './app.module';
   import { SwaggerModule } from '@nestjs/swagger/dist/swagger-module';
